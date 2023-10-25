da4d57750013fc7231001fbc8f1e62dbd1f32d43
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-25 23:38:10 +0000

    Add Home page component (parent component for the landing page)

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
 
+
+//Parent component for landing page
 export default function Home() {
   return (
     <>
